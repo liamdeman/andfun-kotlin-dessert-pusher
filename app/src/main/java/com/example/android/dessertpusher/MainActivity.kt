da8160a3e5c9361f4749bf4d2dfb958fb95e672d
Liam Deman <liam.deman@student.hogent.be>
2022-01-18 20:11:52 +0100

    17. onSaveInstanceState

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -29,6 +29,8 @@ import androidx.lifecycle.LifecycleObserver
 import com.example.android.dessertpusher.databinding.ActivityMainBinding
 import timber.log.Timber
 
+const val KEY_REVENUE = "key_revenue"
+
 class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
@@ -78,6 +80,10 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         }
 
         dessertTimer = DessertTimer(this.lifecycle)
+
+        if (savedInstanceState != null){
+            revenue = savedInstanceState.getInt(KEY_REVENUE)
+        }
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -178,5 +184,11 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onDestroy() {
         super.onDestroy()
         Timber.i("onDestroy Called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_REVENUE, revenue)
+        Timber.i("onSaveInstanceState called")
     }
 }
